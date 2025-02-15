633ebfb4bfb9e1083c694686aaec2c37a7e7c489
Robert Hill <robert.hill@uphillsolutions.tech>
2025-02-15 13:25:38 -0500

    Debugging

--- lua/ssh/connections.lua
+++ lua/ssh/connections.lua
@@ -170,9 +170,10 @@ end
 M.unmount_host = function()
 	if sshfs_job_id then
 		-- Change to home directory before unmounting if inside mount_point
+		vim.notify("Current mount_point: " .. (mount_point or "nil"), vim.log.levels.INFO)
+
 		if mount_point and string.find(vim.loop.cwd(), mount_point, 1, true) == 1 then
 			vim.cmd("cd ~") -- Change to home directory before unmounting
-			vim.cmd("pwd") -- Print new working directory for debugging
 		end
 		-- Ensure mount_point is valid
 		if mount_point and vim.fn.isdirectory(mount_point) == 1 then
