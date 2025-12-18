c38013446e07747a428d5408e2f542fc768ddedb
Robert Hill <robert.hill@uphillsolutions.tech>
2025-12-18 12:18:33 -0500

    refactor: centralize API command setup and expose public methods
    The API command setup logic has been moved from 'lua/sshfs/init.lua' to a new 'Api.setup' function within 'lua/sshfs/api.lua'. This change centralizes the definition of Neovim commands closer to their API implementations.
    
    Additionally, public API methods are now directly exposed on the 'App' object in 'lua/sshfs/init.lua', improving accessibility and consistency when interacting with the plugin's API.

--- lua/sshfs/api.lua
+++ lua/sshfs/api.lua
@@ -325,4 +325,81 @@ Api.browse = Api.files
 --- @deprecated Use explore instead
 Api.change_to_mount_dir = Api.explore
 
+--- Creates API commands for vim api.
+Api.setup = function()
+	vim.api.nvim_create_user_command("SSHConnect", function(opts)
+		if opts.args and opts.args ~= "" then
+			local SSHConfig = require("sshfs.lib.ssh_config")
+			local host = SSHConfig.parse_host(opts.args)
+			Api.connect(host)
+		else
+			Api.connect()
+		end
+	end, { nargs = "?", desc = "Remotely connect to host via picker or command as argument." })
+
+	vim.api.nvim_create_user_command("SSHConfig", function()
+		Api.config()
+	end, { desc = "Edit SSH config files" })
+
+	vim.api.nvim_create_user_command("SSHReload", function()
+		Api.reload()
+	end, { desc = "Reload SSH configuration" })
+
+	vim.api.nvim_create_user_command("SSHDisconnect", function()
+		Api.unmount()
+	end, { desc = "Disconnect from current SSH host" })
+
+	vim.api.nvim_create_user_command("SSHDisconnectAll", function()
+		Api.unmount_all()
+	end, { desc = "Disconnect from all SSH hosts" })
+
+	vim.api.nvim_create_user_command("SSHTerminal", function()
+		Api.ssh_terminal()
+	end, { desc = "Open SSH terminal session to remote host" })
+
+	vim.api.nvim_create_user_command("SSHFiles", function()
+		Api.files()
+	end, { desc = "Browse remote files" })
+
+	vim.api.nvim_create_user_command("SSHLiveFind", function(opts)
+		local pattern = opts.args and opts.args ~= "" and opts.args or nil
+		Api.live_find(pattern)
+	end, { nargs = "?", desc = "Live find on mounted remote host" })
+
+	vim.api.nvim_create_user_command("SSHGrep", function(opts)
+		local pattern = opts.args and opts.args ~= "" and opts.args or nil
+		Api.grep(pattern)
+	end, { nargs = "?", desc = "Search text in remote files" })
+
+	vim.api.nvim_create_user_command("SSHLiveGrep", function(opts)
+		local pattern = opts.args and opts.args ~= "" and opts.args or nil
+		Api.live_grep(pattern)
+	end, { nargs = "?", desc = "Live grep on mounted remote host" })
+
+	vim.api.nvim_create_user_command("SSHExplore", function()
+		Api.explore()
+	end, { desc = "Explore SSH mount" })
+
+	vim.api.nvim_create_user_command("SSHCommand", function(opts)
+		local cmd = opts.args and opts.args ~= "" and opts.args or nil
+		Api.command(cmd)
+	end, { nargs = "?", desc = "Run command on SSH mount (e.g., :SSHCommand tcd)" })
+
+	vim.api.nvim_create_user_command("SSHChangeDir", function()
+		Api.change_dir()
+	end, { desc = "Change directory to SSH mount" })
+
+	-- TODO: Delete these after January 15th
+	-- Deprecated command aliases
+	vim.api.nvim_create_user_command("SSHEdit", function()
+		vim.notify("SSHEdit is deprecated. Use :SSHConfig instead.", vim.log.levels.WARN)
+		Api.config()
+	end, { desc = "Edit SSH config files (deprecated: use SSHConfig)" })
+
+	vim.api.nvim_create_user_command("SSHBrowse", function()
+		vim.notify("SSHBrowse is deprecated. Use :SSHFiles instead.", vim.log.levels.WARN)
+		Api.files()
+	end, { desc = "Browse remote files (deprecated: use SSHFiles)" })
+end
+
 return Api
--- lua/sshfs/init.lua
+++ lua/sshfs/init.lua
@@ -3,86 +3,6 @@
 
 local App = {}
 
---- Creates API commands for vim api.
-App.setup_api_commands = function()
-	local Api = require("sshfs.api")
-
-	-- Create commands
-	vim.api.nvim_create_user_command("SSHConnect", function(opts)
-		if opts.args and opts.args ~= "" then
-			local SSHConfig = require("sshfs.lib.ssh_config")
-			local host = SSHConfig.parse_host(opts.args)
-			Api.connect(host)
-		else
-			Api.connect()
-		end
-	end, { nargs = "?", desc = "Remotely connect to host via picker or command as argument." })
-
-	vim.api.nvim_create_user_command("SSHConfig", function()
-		Api.config()
-	end, { desc = "Edit SSH config files" })
-
-	vim.api.nvim_create_user_command("SSHReload", function()
-		Api.reload()
-	end, { desc = "Reload SSH configuration" })
-
-	vim.api.nvim_create_user_command("SSHDisconnect", function()
-		Api.unmount()
-	end, { desc = "Disconnect from current SSH host" })
-
-	vim.api.nvim_create_user_command("SSHDisconnectAll", function()
-		Api.unmount_all()
-	end, { desc = "Disconnect from all SSH hosts" })
-
-	vim.api.nvim_create_user_command("SSHTerminal", function()
-		Api.ssh_terminal()
-	end, { desc = "Open SSH terminal session to remote host" })
-
-	vim.api.nvim_create_user_command("SSHFiles", function()
-		Api.files()
-	end, { desc = "Browse remote files" })
-
-	vim.api.nvim_create_user_command("SSHLiveFind", function(opts)
-		local pattern = opts.args and opts.args ~= "" and opts.args or nil
-		Api.live_find(pattern)
-	end, { nargs = "?", desc = "Live find on mounted remote host" })
-
-	vim.api.nvim_create_user_command("SSHGrep", function(opts)
-		local pattern = opts.args and opts.args ~= "" and opts.args or nil
-		Api.grep(pattern)
-	end, { nargs = "?", desc = "Search text in remote files" })
-
-	vim.api.nvim_create_user_command("SSHLiveGrep", function(opts)
-		local pattern = opts.args and opts.args ~= "" and opts.args or nil
-		Api.live_grep(pattern)
-	end, { nargs = "?", desc = "Live grep on mounted remote host" })
-
-	vim.api.nvim_create_user_command("SSHExplore", function()
-		Api.explore()
-	end, { desc = "Explore SSH mount" })
-
-	vim.api.nvim_create_user_command("SSHCommand", function(opts)
-		local cmd = opts.args and opts.args ~= "" and opts.args or nil
-		Api.command(cmd)
-	end, { nargs = "?", desc = "Run command on SSH mount (e.g., :SSHCommand tcd)" })
-
-	vim.api.nvim_create_user_command("SSHChangeDir", function()
-		Api.change_dir()
-	end, { desc = "Change directory to SSH mount" })
-
-	-- TODO: Delete these after January 15th
-	-- Deprecated command aliases
-	vim.api.nvim_create_user_command("SSHEdit", function()
-		vim.notify("SSHEdit is deprecated. Use :SSHConfig instead.", vim.log.levels.WARN)
-		Api.config()
-	end, { desc = "Edit SSH config files (deprecated: use SSHConfig)" })
-
-	vim.api.nvim_create_user_command("SSHBrowse", function()
-		vim.notify("SSHBrowse is deprecated. Use :SSHFiles instead.", vim.log.levels.WARN)
-		Api.files()
-	end, { desc = "Browse remote files (deprecated: use SSHFiles)" })
-end
-
 --- Main entry point for plugin initialization.
 ---@param user_opts table|nil User configuration options to merge with defaults
 function App.setup(user_opts)
@@ -114,7 +34,28 @@ function App.setup(user_opts)
 		})
 	end
 
-	App.setup_api_commands()
+	local Api = require("sshfs.api")
+	Api.setup()
 end
+
+-- Expose public API methods on App object for require("sshfs").method() usage
+local Api = require("sshfs.api")
+App.connect = Api.connect
+App.mount = Api.mount
+App.disconnect = Api.disconnect
+App.unmount = Api.unmount
+App.unmount_all = Api.unmount_all
+App.has_active = Api.has_active
+App.get_active = Api.get_active
+App.config = Api.config
+App.reload = Api.reload
+App.files = Api.files
+App.grep = Api.grep
+App.live_grep = Api.live_grep
+App.live_find = Api.live_find
+App.explore = Api.explore
+App.change_dir = Api.change_dir
+App.ssh_terminal = Api.ssh_terminal
+App.command = Api.command
 
 return App
